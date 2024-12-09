bbdbf0b8f0a9277f00bac74dd087e1ecf5988c74
FlyingNugget <ab7948@student.jamk.fo>
2024-12-09 19:22:33 +0200

    tailwind and theming added

---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+}
